370b5a796b09308603ef7ae3f8fbd5efe8630cd3
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-09 21:14:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+   return nested.querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll("ul.ranked-list li");
+
+  for(var i = 0; i < lis.length; i++)
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+}
+
+function deepestChild() {
+  var lis = document.getElementById("grand-node").querySelectorAll("div");
+  return lis[lis.length - 1];
+}
